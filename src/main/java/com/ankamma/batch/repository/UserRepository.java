1365f4fc5650ca436284ece6a9e60cb9d30735d0
apallapu_java <ankamma.java@gmail.com>
2019-11-25 23:44:04 +0530

    done

--- src/main/java/com/ankamma/batch/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ankamma.batch.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ankamma.batch.model.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
